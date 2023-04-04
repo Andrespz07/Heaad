59a1ba812821be344259cf6bf8932cd91ec271ba
Andrespz07 <andresperezortega2001@gmail.com>
2023-04-04 22:51:35 +0200

    exercise finished

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+import cara from '../assets/cara.jpg'
+import cruz from '../assets/cruz.jpg'
+
+const moneda = ref('')
+
+function lanzarMoneda() {
+  const resultado = Math.random() < 0.5 ? cara : cruz
+  moneda.value = resultado
+}
+</script>
+
+<template>
+  <div>
+    <img v-if="moneda === cara" :src="moneda" alt="Moneda cara">
+    <img v-else-if="moneda === cruz" :src="moneda" alt="Moneda cruz">
+    <button @click="lanzarMoneda">Lanzar moneda</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  img {
+    max-width: 200px;
+    max-height: 200px;
+  }
+</style>
